372f35ff1f7144b87c8ef495295eb13d616584d1
Ashok Vishwakarma <ashokvishwakarma1099@gmail.com>
2025-08-29 23:44:37 +0530

    Added UI URL to CORS

--- src/main/java/com/ashok/resume_screener/config/CorsConfig.java
+++ src/main/java/com/ashok/resume_screener/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // apply to all endpoints
-                        .allowedOrigins("http://localhost:4200") // your Angular app URL
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://ai-resume-screener-by-ashok.web.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
